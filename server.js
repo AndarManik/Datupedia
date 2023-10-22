3b07bce6968423d4e0272f1ff82997853118a918
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2023-10-22 01:07:39 -0400

    first commint

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const rateLimit = require("express-rate-limit");
-const WikipediaAPI = require("./WikipediaAPI");
-const TextExtractor = require("./TextExtractor");
-const DatuPage = require("./DatuPage");
-const { connectToDb, getDb } = require("./MongoAPI");
+const WikipediaAPI = require("./APIs/WikipediaAPI");
+const DatuPage = require("./DatuPageHandlers/DatuPage");
+const { connectToDb } = require("./APIs/MongoAPI");
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
 const WebSocket = require("ws");
-
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const wikipediaAPI = new WikipediaAPI();
-const textExtractor = new TextExtractor();
+const { v4: uuidv4 } = require("uuid");
+const users = [];
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 // Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -60,53 +60,173 @@ app.get("/suggestions", async (req, res) => {
   res.json(suggestions);
 });
 
-app.get("/:pagename/home", async (req, res) => {
+app.get("/datu/:pagename", async (req, res) => {
   try {
-    const pagename = req.params.pagename;
-
-    // Read the Datupage.html file
+    const pagename = decodeURIComponent(
+      req.params.pagename.replaceAll("_", " ")
+    );
     const filePath = path.join(__dirname, "public/Datupage.html");
     const fileContent = fs.readFileSync(filePath, "utf-8");
-
-    // Replace placeholder with the actual page name
-    const renderedHtml = fileContent.replace(/{{pagename}}/g, pagename);
-
-    // Send the HTML file to the client
-    res.write(renderedHtml);
-
-    // Notify WebSocket clients that fetching has started
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(`Fetching data for ${pagename}...`);
-      }
-    });
-
-    const datuPage = new DatuPage(
-      pagename,
-      getDb(),
-      wikipediaAPI,
-      textExtractor
-    );
-    await datuPage.fetchData();
-    console.log("fetchfinished");
-    // Notify WebSocket clients that fetching is complete
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(`Data fetched: ${datuPage.displayFetchedData()}`);
-      }
-    });
-
-    res.end();
+    let renderedHtml = fileContent.replace(/{{pagename}}/g, pagename);
+    renderedHtml = renderedHtml.replace(/{{userID}}/g, uuidv4());
+    res.send(renderedHtml);
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
   }
 });
 
+app.get("/wiki/:pagename", async (req, res) => {
+  try {
+    const pagename = decodeURIComponent(
+      req.params.pagename.replaceAll("_", " ")
+    );
+    const page = await wikipediaAPI.getContent(pagename);
+    res.send(page);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+const interval = setInterval(() => {
+  wss.clients.forEach((ws) => {
+    ws.send(JSON.stringify({ status: "ping" }));
+  });
+}, 10000); // Send "ping" every 10 seconds
+
+wss.on("connection", (ws) => {
+  ws.on("message", async (message) => {
+    const parsedMessage = JSON.parse(message);
+    user = users[parsedMessage.userId];
+
+    switch (parsedMessage.type) {
+      case "InitializeDatuPage":
+        initializeDatuPage(ws, parsedMessage, user);
+        break;
+      case "Loading data stream":
+        loadingDataStream(ws, user);
+        break;
+      case "GetClusterData":
+        await getClusterData(ws, parsedMessage, user);
+        break;
+      case "Cluster data stream":
+        clusterDataStream(ws, user);
+        break;
+      case "Get recommendation":
+        await getRecommendation(ws, parsedMessage, user);
+        break;
+      case "pong":
+        // No operation needed for "pong" type.
+        break;
+    }
+  });
+});
+
+function initializeDatuPage(ws, parsedMessage, user) {
+  const datuPageInstance = new DatuPage(parsedMessage.pageId);
+  user = {
+    connection: ws,
+    datuPageInstance,
+  };
+  users[parsedMessage.userId] = user;
+  datuPageInstance.fetchData();
+  ws.send(
+    JSON.stringify({
+      status: "success",
+      message: "Loading state",
+      state: "Loading...",
+    })
+  );
+}
+
+function loadingDataStream(ws, user) {
+  const state = user.datuPageInstance.getState();
+  if (user.datuPageInstance.isFetchDone()) {
+    if (user.datuPageInstance.isLargeEnough()) {
+      ws.send(
+        JSON.stringify({
+          status: "success",
+          message: "New DatuPage created",
+        })
+      );
+      return;
+    } else {
+      ws.send(
+        JSON.stringify({
+          status: "success",
+          message: "Not large enough",
+        })
+      );
+      return;
+    }
+  }
+  ws.send(
+    JSON.stringify({
+      status: "success",
+      message: "Loading state",
+      state,
+    })
+  );
+}
+
+async function getClusterData(ws, parsedMessage, user) {
+  if (user && user.datuPageInstance) {
+    if (!(await user.datuPageInstance.has(parsedMessage.position))) {
+      return;
+    }
+    if (user.datuPageInstance.isGenerating()) {
+      return;
+    }
+    user.datuPageInstance.position = parsedMessage.position;
+    await user.datuPageInstance.generatePage();
+
+    ws.send(
+      JSON.stringify({
+        status: "success",
+        message: `generating page`,
+      })
+    );
+  }
+}
+
+function clusterDataStream(ws, user) {
+  const wikitext = user.datuPageInstance.getWikiText();
+  if (user.datuPageInstance.isClusterFinished()) {
+    const parsedText = user.datuPageInstance.getProcessedWikiText();
+    ws.send(
+      JSON.stringify({
+        status: "success",
+        message: "Cluster data finished",
+        clusterData: `${parsedText}`,
+      })
+    );
+    return;
+  }
+  ws.send(
+    JSON.stringify({
+      status: "Processing",
+      message: `Cluster data stream`,
+      clusterData: `${wikitext}`,
+    })
+  );
+}
+
+async function getRecommendation(ws, parsedMessage, user) {
+  const pageName = parsedMessage.pageId;
+  const recommendation = await wikipediaAPI.getMoreLike(pageName);
+  console.log(recommendation);
+  ws.send(
+    JSON.stringify({
+      status: "success",
+      message: "Cluster data finished",
+      clusterData: recommendation,
+    })
+  );
+}
+
 // Centralized Error Handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
-
-
